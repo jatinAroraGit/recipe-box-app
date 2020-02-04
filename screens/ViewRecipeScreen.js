03947856bb812cd1a79b55c039a434f0e479d83b
Sanghyuk Lee <givemesomefoodplz@gmail.com>
2020-02-03 23:46:27 -0500

    Added ViewRecipeScreen

---
+++ screens/ViewRecipeScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+import { Title, Headline, Subheading, Surface, Button, Drawer, Appbar } from 'react-native-paper';
+import {
+  Image,
+  ImageBackground,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView
+} from 'react-native';
+import TopNavbar from '../components/TopNavbar';
+
+export default function PageNotFound() {
+  return (
+    <SafeAreaView style={{flex:3}}>
+  <TopNavbar title='Home'></TopNavbar>
+    <ScrollView>
+      <ViewRecipe style={{margin:10}}>Not Found. Check back later.</ViewRecipe>
+    </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+PageNotFound.navigationOptions = {
+  header: null,
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+  },
+});
