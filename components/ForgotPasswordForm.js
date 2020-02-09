b819d09c4a9bfdfe2fd29f2bda646928485753d4
Patrick <shyskream@hotmail.com>
2020-02-09 17:14:59 -0500

    Updated 'forgot password' with placeholder security question

--- components/ForgotPasswordForm.js
+++ components/ForgotPasswordForm.js
@@ -49,29 +49,46 @@ const styles = StyleSheet.create({
 
 function ForgotPasswordForm({ props }) {
   var auth = Firebase.auth();
-  const { control, handleSubmit, errors } = useForm({ mode: 'onChange' });
+  var securityQuestion = "What is my favorite color? (Red)";
+  var securityAnswer = "Red";
+  const { control, handleSubmit, errors, setError } = useForm({ mode: 'onChange' });
   const onSubmit = data => {
 
     console.log(data);
 
-    if (data.email) {
-
+    if (data.email && data.answer) {
       console.log('Valid email entered.');
-      //TODO:  implement security questions here before email gets sent.
-      auth.sendPasswordResetEmail(data.email).then(function() {
-        // Email sent.
-      console.log('password recovery email sent');
-      }).catch(function(error) {
-        // An error happened.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+
+      if(data.answer == securityAnswer){//TODO change 'true' to checking for the email in the database.
+      //TODO read security questions from database instead of hardcoding.
+        console.log('Valid security question answer');
+
       
+        auth.sendPasswordResetEmail(data.email).then(function() {
+          // Email sent.
+        console.log('password recovery email sent');
+        props.navigate('Home');
+
+        }).catch(function(error) {
+          // An error happened.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          
+      console.log('No user');
+      setError("noUser", 'no user', "no account uses this email");
+        });
+
+      } else {
+        console.log('Invalid security question answer');
+        setError("wrongAnswer", 'wrong answer', "security question answer is incorrect");
+
+      }
     } else {
 
-      console.log('No email entered');
+      console.log('Please fill all fields');
+      setError("missingData", 'missing data', "some fields were left blank");
 
     }
 
@@ -92,9 +109,21 @@ function ForgotPasswordForm({ props }) {
         name="email"
         control={control}
         onChange={onChange}
-        rules={{ pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9][a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ }}
+        rules={{ required: true, pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9][a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ }}
       />
       {errors.email && <Subheading style={{ color: '#BF360C' }}>Invalid Email.</Subheading>}
+      {errors.noUser && <Subheading style={{ color: '#BF360C' }}>No account exists using that email.</Subheading>}
+      
+      <Subheading style={styles.label}>Security Question</Subheading>
+      <Subheading style={styles.label}>{securityQuestion}</Subheading>
+      <Controller
+        as={<TextInput style={styles.input} />}
+        name="answer"
+        control={control}
+        onChange={onChange}
+      />
+      {errors.wrongAnswer && <Subheading style={{ color: '#BF360C' }}>Answer is incorrect.</Subheading>}
+      {errors.missingData && <Subheading style={{ color: '#BF360C' }}>Please fill in both fields.</Subheading>}
 
       <Button style={{ marginHorizontal: 10, marginTop: 20 }} mode="contained" onPress={handleSubmit(onSubmit)}>
         Send
