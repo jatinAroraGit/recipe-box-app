2423b5a96df8e59b0fc81173b318153a59deb44a
Sanghyuk Lee <givemesomefoodplz@gmail.com>
2020-02-09 16:01:40 -0500

    Changes

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -30,7 +30,7 @@ class SearchScreen extends React.Component {
           <View style={{ marginStart:10, marginTop: 10, marginEnd:10, position: 'relative', top: 0, left: 0, right: 0, bottom: 0 , justifyContent: 'center', alignItems: 'center', borderWidth: 0, borderRadius: 30, overflow: "hidden"}}>
             
               <SearchForm props={this.props.navigation}></SearchForm>
-              <Button color='#FFFFFF' style={{alignSelf: 'center',backgroundColor:'grey', margin: 20}} onPress={() => this.props.navigation.navigate('Recipe')}>
+              <Button color='#FFFFFF' style={{alignSelf: 'center',backgroundColor:'grey', margin: 20}} onPress={() => this.props.navigation.navigate('Results')}>
                         Results
                     </Button>
             
