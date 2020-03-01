6ddab7dac5cab26f4b707cccd89c2379c32590a3
Sanghyuk Lee <givemesomefoodplz@gmail.com>
2020-03-01 17:01:05 -0500

    Connected ViewRecipe with ShoppingList

--- components/ViewRecipe.js
+++ components/ViewRecipe.js
@@ -1,16 +1,122 @@
-import React from 'react';
-import {StyleSheet, Text, View, SafeAreaView, Image, ScrollView} from "react-native";
-import {Ionicons, MaterialIcons} from '@expo/vector-icons'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, Button } from "react-native";
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+import axios from 'axios';
 
-function ViewRecipe({navigation, recipeDetail}) {
-      
+function ViewRecipe({ navigation, recipeDetail }) {
+
+    // console.log('navigation in ViewRecipe - start');
+    // console.log(navigation);
+    // console.log('navigation in ViewRecipe - end')
+
+    console.log('Showing the id of the recipe - start');
+    console.log(recipeDetail.id)
+    console.log('Showing the id of the recipe - end')
+
     const baseUri = `https://spoonacular.com/recipeImages/`;
 
-     console.log('start - yo');
-     console.log(recipeDetail.image);
-     console.log('end - yo')
+    const [ingred, setIngred] = useState([]); //setIngred is such a '=' sign to connect ingred and ingredientsArray to pass the ingredientsArray to ingred.
+    var ingredientsArray = [];
 
-    return(
+    useEffect(() => {
+        console.log('useEffect has been called');
+        let ingredients = "apples,+flour,+sugar"
+        let apiKey = require('../configure/apiKey.json');
+        let recipeId = recipeDetail.id;
+        if (ingredients) {
+            console.log('If statement is called');
+            // axios.get('https://api.spoonacular.com/recipes/findByIngredients?apiKey=' + apiKey.key + '&ingredients=' + ingredients + '&number=2')
+            axios.get('https://api.spoonacular.com/recipes/' + recipeId + '/analyzedInstructions?apiKey=' + apiKey.key) //Need to change the id and apiKey
+                .then(res => {
+                    console.log('axios is called');
+                    const ingreds = res.data[0].steps;
+                    console.log('HiHello');
+                    console.log(res.data[0]);
+                    console.log('HiBye');
+                    // console.log(items)
+                    extractIngredients(ingreds);
+                })
+        }
+
+
+    }, []);
+
+    const extractIngredients = (ingreds) => {
+
+        for (let i = 0; i < ingreds.length; i++) {
+            for (let j = 0; j < ingreds[i].ingredients.length; j++) {
+                ingreds[i].ingredients[j].count = 0;
+                ingredientsArray.push(ingreds[i].ingredients[j]); //IngredientsArray currently holds a collection of ingredients' objects
+            }
+        }
+
+        ingredientsArray = ingredientsArray.filter((ingredElement, index, self) =>
+            index === self.findIndex((t) => (
+                t.id === ingredElement.id
+            ))
+        )
+
+        //ingredElement is each element of ingredientsArray, index is 0,1,2..., self is ingredientsArray, t is basically same as the ingredElement
+        // if the index is not matching returned number made by self.findIndex, it would not return. 
+
+        console.log('Hello no dupes', ingredientsArray)
+
+        setIngred(ingredientsArray);
+
+
+        /*
+        ingred - an array of steps. 
+        each step is another object, object has an array of ingredients. 
+        the ingredients array has many objects that has name as name of ingred. 
+        */
+    };
+
+    const incrementCountHandler = (incomingIngred) => {
+
+        let ingredsCopy = Array.from(ingred);
+
+        ingredsCopy.forEach((curr) => {
+            if (curr.id === incomingIngred.id) {
+                if (curr.count >= 0) {
+                    curr.count = curr.count + 1;
+                }
+            }
+        })
+
+        setIngred(ingredsCopy);
+    };
+
+
+    const decrementCountHandler = (incomingIngred) => {
+
+        let ingredsCopy = Array.from(ingred);
+
+        ingredsCopy.forEach((curr) => {
+            if (curr.id === incomingIngred.id) {
+                if (curr.count > 0) {
+                    curr.count = curr.count - 1;
+                }
+            }
+
+        })
+        setIngred(ingredsCopy);
+    };
+
+    const makeJsontoObject = (JsonObject) => {
+        if (JsonObject.length != 0) {
+            for (let i = 0; i < JsonObject.length; i++) {
+                JsonObject[i] = JSON.stringify(JsonObject[i]);
+            }
+        }else {
+            console.log('Hey you should pick at least one of the ingredients.');
+        }
+
+        return JsonObject;
+
+    }
+
+
+    return (
         <SafeAreaView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={styles.titleBar}>
@@ -18,41 +124,41 @@ function ViewRecipe({navigation, recipeDetail}) {
                     <Ionicons name="md-more" size={24} color="rgb(82,87,93)"></Ionicons>
                 </View>
 
-                <View style={{alignSelf: "center"}}>
+                <View style={{ alignSelf: "center" }}>
                     <View style={styles.profileImage}>
-                        <Image source={`${baseUri}${recipeDetail.image}`} style={styles.image} resizeMode="center"></Image> 
+                        <Image source={`${baseUri}${recipeDetail.image}`} style={styles.image} resizeMode="center"></Image>
                     </View>
                     <View style={styles.dm}>
                         <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
                     </View>
-                    <View style={styles.active}></View> 
+                    <View style={styles.active}></View>
                     <View style={styles.add}>
-                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{marginTop: 6, marginLeft: 2}}></Ionicons> 
+                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
                     </View>
                 </View>
 
 
                 <View style={styles.infoContainer}>
-                    <Text style={[styles.text, {fontWeight: "200", fontSize: 36}]}>{recipeDetail.title}</Text>
-                    <Text style={[styles.text, {color: "#AEB5BC", fontSize: 14}]}>World Best!</Text>
+                    <Text style={[styles.text, { fontWeight: "200", fontSize: 15 }]}>{recipeDetail.title}</Text>
+                    <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>World Best!</Text>
                 </View>
 
                 <View style={styles.statsContainer}>
                     <View style={styles.statsBox}>
-                        <Text style={[styles.text, {fontSize: 24}]}>483</Text>
+                        <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
                         <Text style={[styles.text, styles.subText]}>Posts</Text>
                     </View>
-                    <View style={[styles.statsBox, {borderColor: "#DFDBCB", borderLeftWidth: 1, borderRightWidth: 1}]}>
-                        <Text style={[styles.text, {fontSize: 24}]}>45,844</Text>
+                    <View style={[styles.statsBox, { borderColor: "#DFDBCB", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>45,844</Text>
                         <Text style={[styles.text, styles.subText]}>Followers</Text>
                     </View>
                     <View style={styles.statsBox}>
-                        <Text style={[styles.text, {fontSize: 24}]}>302</Text>
+                        <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
                         <Text style={[styles.text, styles.subText]}>Following</Text>
                     </View>
                 </View>
 
-                <View style={{marginTop: 32}}>
+                <View style={{ marginTop: 32 }}>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <View style={styles.mediaImageContainer}>
                             <Image source={require("../assets/images/background.jpg")} style={styles.image} resizeMode="cover"></Image>
@@ -66,22 +172,55 @@ function ViewRecipe({navigation, recipeDetail}) {
 
                     </ScrollView>
                     <View style={styles.mediaCount}>
-                        <Text style={[styles.text, {fontSize: 24, color: "#DFD8C8", fontWeight: "300"}]}>70</Text>
-                        <Text style={[styles.text, {fontSize: 12, color: "#DFD8C8", textTransform: "uppercase"}]}>Media</Text>
+                        <Text style={[styles.text, { fontSize: 24, color: "#DFD8C8", fontWeight: "300" }]}>70</Text>
+                        <Text style={[styles.text, { fontSize: 12, color: "#DFD8C8", textTransform: "uppercase" }]}>Media</Text>
                     </View>
                 </View>
+
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Ingredients</Text>
+                    {console.log(ingred, 'removed dupes')}
+
+
+                    {ingred.map((oneIngred, index) => {
+                        return (
+                            <View key={oneIngred.id}>
+                                <Text>{index + 1}: {oneIngred.name}</Text>
+                                <Button title='-' onPress={() => {
+                                    decrementCountHandler(oneIngred);
+                                }}></Button>
+                                <Text>{oneIngred.count}</Text>
+                                <Button title='+' onPress={() => {
+                                    incrementCountHandler(oneIngred)
+                                }}></Button>
+                            </View>
+
+                        )
+                    })}
+
+
+
+
+                    <Button title="View Shopping List" onPress={() => {
+                        navigation.navigate('Shopping', makeJsontoObject(ingred));
+                        console.log('Button is clicked');
+                        console.log(ingred);
+                        console.log('Bye Button');
+                    }}></Button>
+
+                </View >
 
 
                 <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
 
-                <View style={{alignItems: "center"}}>
+                <View style={{ alignItems: "center" }}>
                     <View style={styles.recentItem}>
                         <View style={styles.recentItemIndicator}></View>
-                        <View style={{width: 250}}>
-                            <Text style={[styles.text, {color: "rgb(65,68,75)", fontWeight: "300"}]}>
-                                Started Following{" "} 
-                                <Text style={{fontWeight: "400"}}>
-                                    Jason, Jatin, Sanghyuk Lee, Narma, Patrick <Text style={{fontWeight: "400"}}>GroupQuattro</Text>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "rgb(65,68,75)", fontWeight: "300" }]}>
+                                Started Following{" "}
+                                <Text style={{ fontWeight: "400" }}>
+                                    Jason, Jatin, Sanghyuk Lee, Narma, Patrick <Text style={{ fontWeight: "400" }}>GroupQuattro</Text>
                                 </Text>
                             </Text>
                         </View>
@@ -89,13 +228,12 @@ function ViewRecipe({navigation, recipeDetail}) {
 
                     <View style={styles.recentItem}>
                         <View style={styles.recentItemIndicator}></View>
-                        <View style={{width: 250}}>
-                            <Text style={[styles.text, {color: "#41444B", fontWeight: "300"}]}>
-                                Started Following <Text style={{fontWeight: "400"}}> Recipe2 </Text>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started Following <Text style={{ fontWeight: "400" }}> Recipe2 </Text>
                             </Text>
                         </View>
                     </View>
-                    {/* <Button title="Go to Home" onPress={() => navigation.navigate('Results')} /> */}
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -206,7 +344,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         borderRadius: 12,
         shadowColor: "rgba(0, 0 ,0 ,0.38)",
-        shadowOffset: {width: 0, height: 10},
+        shadowOffset: { width: 0, height: 10 },
         shadowRadius: 20,
         shadowOpacity: 1
 
--- screens/ShoppingListScreen.js
+++ screens/ShoppingListScreen.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { View, SafeAreaView, ScrollView, Text, Button } from 'react-native';
 //import { Button } from 'react-native-paper'
 import TopNavbar from '../components/TopNavbar';
-import RegisterForm from '../components/RegisterForm';
-import ViewRecipe from '../components/ViewRecipe';
+import ShoppingList from '../components/ShoppingList';
 
 
 class ShoppingListScreen extends React.Component {
@@ -19,6 +18,8 @@ class ShoppingListScreen extends React.Component {
         this.setState({login: childData});
         console.log("login complete!")
     }
+
+    
   
   render() {
     
@@ -28,10 +29,13 @@ class ShoppingListScreen extends React.Component {
         <TopNavbar title='Search'></TopNavbar>
         <ScrollView >
           <View style={{ marginStart:10, marginTop: 10, marginEnd:10, position: 'relative', top: 0, left: 0, right: 0, bottom: 0 , justifyContent: 'center', alignItems: 'center', borderWidth: 0, borderRadius: 30, overflow: "hidden"}}>  
+            {/* {console.log('This is showing ingredSent - Start')}
+            {console.log(ingred)}
+            {console.log('This is showing ingredSent - end')} */}
             {console.log('This is start-ViewRecipeScreen')}
-            {console.log(this.state.navigation.state.params)}
+            {console.log(JSON.parse(this.state.navigation.state.params[0]))}
             {console.log('This is end-ViewRecipeScreen')}
-            <ShoppingList navigation={this.props.navigation}></ShoppingList>
+            <ShoppingList navigation={this.props.navigation} ingredSent={this.props.navigation.state.params}></ShoppingList>
           </View>
         </ScrollView> 
       </SafeAreaView>
